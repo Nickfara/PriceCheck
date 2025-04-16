e537165fc0595a8067919e11d4c9c99ace30cb2e
Nickfara <40189178+Nickfara@users.noreply.github.com>
2025-04-16 11:16:06 +0500

    Лёгкие правки

--- main/testers.py
+++ main/testers.py
@@ -4,11 +4,6 @@ s = requests.Session()  # Создание сессии
 s.headers.update({'Tele2-User-Agent': 'mytele2-app/5.11.0', 'User-Agent': 'okhttp/5.3.1'})  # Заголовок с данными
 
 
-#s.headers.update(SECURITY_BYPASS_HEADERS)
-
-#response = s.get('https://ekt.t2.ru/api/cart?siteId=siteEKT')
-
-#print(response)
 
 response = s.post('https://ekt.t2.ru/api/validation/number/79923415301', json={'sender': 'Tele2'})
 
