7afa6f8e174c9a31eeab3b6a5d8c0c4a6d074102
Nickfara <40189178+Nickfara@users.noreply.github.com>
2025-06-16 15:03:59 +0500

    г

--- main/PriceCheck/parse_kuper.py
+++ main/PriceCheck/parse_kuper.py
@@ -48,7 +48,7 @@ def login_and_get_cookies(phone_number: str) -> dict:
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver = Firefox()
     driver.set_page_load_timeout(6)
     try:
         print('Ожидание начато.')
@@ -58,22 +58,61 @@ def login_and_get_cookies(phone_number: str) -> dict:
 
 
     try:
-        # Нажатие кнопки 'Войти'
-        login_btn = driver.find_element(By.XPATH, '//*[@id="__next"]/div[1]/div/header/div/div[2]/div/div[5]/div/button')
-        login_btn.click()
-        time.sleep(1.5)
-        # Нажатие кнопки 'Войти по Сбер ID'
-        sber_id_btn = driver.find_element(By.XPATH, '//*[@id="__next"]/div[2]/div[2]/div/div/div[2]/div/form/div[2]/button')
-        sber_id_btn.click()
-        time.sleep(2)
-        # Отключение получения рекламы.
-        ad_disable = driver.find_element(By.XPATH, '//*[@id="desktopContainer"]/div[1]/div/div[2]/div[2]/div/label/span')
-        ad_disable.click()
-        time.sleep(1)
-        # Ввод номера телефона
-        login_input = driver.find_element(By.XPATH, '//*[@id="desktopContainer"]/div[1]/div/form/div/input')
-        login_input.click()
-        login_input.send_keys(phone_number)
+        enter = True
+        while True:
+            try:
+                # Скипаем рекламу
+                skip_ad_btn = driver.find_element(By.XPATH, '/html/body/div[6]/div/div[2]/header/button/div')
+                skip_ad_btn.click()
+                break
+            except:
+                try:
+                    # Нажатие кнопки 'Войти'
+                    login_btn = driver.find_element(By.XPATH,
+                                                    '//*[@id="__next"]/div[1]/div/header/div/div[2]/div/div[5]/div/button')
+                    login_btn.click()
+                    enter = False # Выключение повторныъ попыток нажать кнопку войти
+                    break
+                except:
+                    time.sleep(1)
+
+        while enter:
+            try:
+                # Нажатие кнопки 'Войти'
+                login_btn = driver.find_element(By.XPATH, '//*[@id="__next"]/div[1]/div/header/div/div[2]/div/div[5]/div/button')
+                login_btn.click()
+                break
+            except:
+                time.sleep(1)
+
+        while True:
+            try:
+                # Нажатие кнопки 'Войти по Сбер ID'
+                sber_id_btn = driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div[2]/div/div/div[2]/div/form/div[2]/button")
+                sber_id_btn.click()
+                break
+            except:
+                time.sleep(1)
+
+        while True:
+            try:
+                # Отключение получения рекламы.
+                ad_disable = driver.find_element(By.XPATH, '//*[@id="desktopContainer"]/div[1]/div/div[2]/div[2]/div/label/span')
+                ad_disable.click()
+                break
+            except:
+                time.sleep(1)
+
+        while True:
+            try:
+                # Ввод номера телефона
+                login_input = driver.find_element(By.XPATH, '//*[@id="desktopContainer"]/div[1]/div/form/div/input')
+                login_input.click()
+                login_input.send_keys(phone_number)
+                break
+            except:
+                time.sleep(1)
+
         try:
             send_code = driver.find_element(By.XPATH, '//*[@id="desktopContainer"]/div[1]/div/form/button')
             send_code.click()
@@ -92,6 +131,7 @@ def login_and_get_cookies(phone_number: str) -> dict:
 
     finally:
         driver.quit()
+
 
 def get_valid_session(phone_number=LOGIN, cookies_file=COOKIES_FILE) -> Session:
     """Проверка куки файлы на актуальность и получение актуального объекта сессии.
@@ -147,7 +187,7 @@ def get_valid_session(phone_number=LOGIN, cookies_file=COOKIES_FILE) -> Session:
         # else:
         #     print("[!] Cookies устарели. Требуется переавторизация.")
 
-        check_response = session.get("https://kuper.ru/api/v3/external_partners/services")
+        check_response = session.get("https://kuper.ru/api/shipments/active")
         print(check_response)
         if check_response.ok:
             return session
@@ -169,67 +209,67 @@ def get_valid_session(phone_number=LOGIN, cookies_file=COOKIES_FILE) -> Session:
     return session
 
 
-
 def code_input_callback():
     return input()
 
 
-def coop_sberid_login(phone_number: str=LOGIN, code_input_callback=code_input_callback) -> dict:
-    """
-    Авторизация на Купер.маркет через СБЕР ID.
-    :param phone_number: номер телефона в формате +7ХХХХХХХХХХ
-    :param code_input_callback: функция, возвращающая смс-код
-    :return: cookies (dict)
-    """
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    driver = Firefox()
+def find(cookies_file=COOKIES_FILE, text='Сахар'):
+    link = 'https://kuper.ru/api/web/v1/products'
 
-    try:
-        driver.get("https://kuper.ru/")  # 1. Заход на Купер
-
-        # 2. Нажимаем "Войти"
-        WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Войти')]"))).click()
-
-        # 3. Нажимаем "Сбер ID"
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Сбер ID')]"))).click()
-
-        # 4. Переключаемся на вкладку СБЕР ID
-        time.sleep(2)
-        driver.switch_to.window(driver.window_handles[-1])
-        WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, "login")))
-
-        # 5. Ввод номера телефона
-        driver.find_element(By.ID, "login").send_keys(phone_number)
-        driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-
-        # 6. Ввод кода из SMS
-        time.sleep(2)
-        code = code_input_callback()
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "otp")))
-        driver.find_element(By.ID, "otp").send_keys(code)
-
-        # 7. Завершение авторизации
-        WebDriverWait(driver, 15).until(lambda d: len(driver.window_handles) == 1)
-        driver.switch_to.window(driver.window_handles[0])
-
-        # Убедимся, что пользователь залогинен
-        WebDriverWait(driver, 15).until(
-            EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Мой профиль')]"))
-        )
-
-        # Получение cookies
-        cookies = {c['name']: c['value'] for c in driver.get_cookies()}
-
-        print("✅ Авторизация прошла успешно.")
-        return cookies
-
-    except Exception as e:
-        print("❌ Ошибка авторизации:", e)
-        return {}
-
-    finally:
-        driver.quit()
+    data = {
+        'ads_identity': {'ads_promo_identity': {'placement_uid': "cg4tmrigsvdveog2p240", 'site_uid': "c9qep2jupf8ugo3scn10"}},
+        'filter': [
+            {"key": "brand", "values": []},
+            {"key": "permalinks","values": []},
+            {"key": "price","values": []},
+            {"key": "discounted","values": []},
+            {"key": "root_category","values": []}
+        ],
+        'page': "1",
+        'per_page': "24",
+        'q': text,
+        'store_id': "66",
+        'tenant_id': "sbermarket"
+    }
 
 
-coop_sberid_login()
+    headers = {
+        'accept':
+        'application/json, text/plain, */*',
+        'accept-encoding':
+        'gzip, deflate, br, zstd',
+        'accept-language':
+        'ru,en;q=0.9',
+        'baggage':
+        'sentry-environment=client,sentry-release=r25-06-11-1896-176f596d,sentry-public_key=f9d0a0afb8d5420bb353a190580ae049,sentry-',
+        'referer': 'https://kuper.ru/metro/search?ads_identity.ads_promo_identity.placement_uid=cg4tmrigsvdveog2p240&ads_identity.ads_promo_identity.site_uid=c9qep2jupf8ugo3scn10&anonymous_id=43df9a32-9a0d-433d-9970-ab1091dd2d11&keywords=%D0%A1%D0%90%D0%A5%D0%90%D0%A0%5C&sid=66',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'sentry-trace': '50aaeaf67a1b48bbb653790a065fab18-8669d2535644039c-0',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.3.1 Safari/605.1.15'
+    }
+
+    with open(cookies_file) as f:
+        data_temp = json.load(f)
+        cookies = data_temp['shops']
+
+
+    session = requests.Session()
+    session.headers.update(headers)
+    session.cookies.update(cookies)
+    token = session.get('https://kuper.ru/api/v3/external_partners/services', json={
+    "services": [
+        {
+            "id": 1,
+            "kind": "sber_prime"
+        }
+    ]
+})
+    print(token.text)
+    response = session.post(link, data=data)
+    #print(response.text)
+
+find()
+
+
